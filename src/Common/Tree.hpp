808e58a56f1589ff70ac1899780c9af6e06c6331
yaroslavKonst <yaroslav.konst@yandex.ru>
2025-12-07 16:38:47 +0500

    Added AVL tree.

---
+++ src/Common/Tree.hpp
@@ -0,0 +1,690 @@
+#ifndef _TREE_HPP
+#define _TREE_HPP
+
+#include <cstdint>
+
+#include "Exception.hpp"
+
+template <typename KT, typename VT>
+class Tree
+{
+public:
+	struct Entry
+	{
+		KT Key;
+		VT Value;
+
+		Entry *Parent;
+		Entry *Left;
+		Entry *Right;
+
+		int32_t Depth;
+
+		Entry()
+		{
+			memset(this, 0, sizeof(*this));
+		}
+
+		~Entry()
+		{
+			if (Left) {
+				delete Left;
+			}
+
+			if (Right) {
+				delete Right;
+			}
+		}
+	};
+
+	Tree();
+	~Tree();
+
+	Entry *FindEntry(const KT &key);
+
+	bool AddEntry(const KT &key, const VT &value);
+	void RemoveEntry(Entry *address);
+
+	VT &GetEntry(Entry *address);
+
+	Entry *FindSmallest(const KT &key);
+	Entry *FindBiggest(const KT &key);
+
+	Entry *FindSmallest();
+	Entry *FindBiggest();
+
+	Entry *Next(Entry *address);
+	Entry *Previous(Entry *address);
+
+private:
+	Entry *_root;
+
+	void RotateLeft(Entry *address);
+	void RotateRight(Entry *address);
+
+	void RollUp(Entry *address);
+	void Swap(Entry *address1, Entry *address2);
+};
+
+template <typename KT, typename VT>
+Tree<KT, VT>::Tree()
+{
+	_root = nullptr;
+}
+
+template <typename KT, typename VT>
+Tree<KT, VT>::~Tree()
+{
+	if (_root) {
+		delete _root;
+	}
+}
+
+template <typename KT, typename VT>
+typename Tree<KT, VT>::Entry *Tree<KT, VT>::FindEntry(const KT &key)
+{
+	Entry *current = _root;
+
+	while (current) {
+		if (key == current->Key) {
+			return current;
+		}
+
+		if (key < current->Key) {
+			current = current->Left;
+		} else {
+			current = current->Right;
+		}
+	}
+
+	return nullptr;
+}
+
+template <typename KT, typename VT>
+bool Tree<KT, VT>::AddEntry(const KT &key, const VT &value)
+{
+	Entry **current = &_root;
+	Entry *parent = nullptr;
+
+	while (*current) {
+		if (key == (*current)->Key) {
+			return false;
+		}
+
+		parent = *current;
+
+		if (key < (*current)->Key) {
+			current = &(*current)->Left;
+		} else {
+			current = &(*current)->Right;
+		}
+	}
+
+	Entry *entry = new Entry;
+	entry->Key = key;
+	entry->Value = value;
+	entry->Parent = parent;
+	entry->Depth = 0;
+
+	*current = entry;
+
+	RollUp(*current);
+
+	return true;
+}
+
+template <typename KT, typename VT>
+void Tree<KT, VT>::RemoveEntry(Entry *address)
+{
+	if (!address->Left) {
+		Entry *parent = address->Parent;
+
+		if (parent) {
+			if (parent->Left == address) {
+				parent->Left = address->Right;
+			} else if (parent->Right == address) {
+				parent->Right = address->Right;
+			} else {
+				THROW("Parent entry does not contain "
+					"subtree address.");
+			}
+		} else {
+			_root = address->Right;
+		}
+
+		if (address->Right) {
+			address->Right->Parent = address->Parent;
+		}
+
+		address->Left = nullptr;
+		address->Right = nullptr;
+		delete address;
+		RollUp(parent);
+	} else if (!address->Right) {
+		Entry *parent = address->Parent;
+
+		if (parent) {
+			if (parent->Left == address) {
+				parent->Left = address->Left;
+			} else if (parent->Right == address) {
+				parent->Right = address->Left;
+			} else {
+				THROW("Parent entry does not contain "
+					"subtree address.");
+			}
+		} else {
+			_root = address->Left;
+		}
+
+		if (address->Left) {
+			address->Left->Parent = address->Parent;
+		}
+
+		address->Left = nullptr;
+		address->Right = nullptr;
+		delete address;
+		RollUp(parent);
+	} else {
+		Entry *replacement;
+
+		if (address->Left->Depth > address->Right->Depth) {
+			replacement = address->Left;
+
+			while (replacement->Right) {
+				replacement = replacement->Right;
+			}
+		} else {
+			replacement = address->Right;
+
+			while (replacement->Left) {
+				replacement = replacement->Left;
+			}
+		}
+
+		Swap(replacement, address);
+		RemoveEntry(address);
+	}
+}
+
+template <typename KT, typename VT>
+VT &Tree<KT, VT>::GetEntry(Entry *address)
+{
+	return address->Value;
+}
+
+template <typename KT, typename VT>
+typename Tree<KT, VT>::Entry *Tree<KT, VT>::FindSmallest(const KT &key)
+{
+	Entry *minAddress = nullptr;
+	KT minKey;
+
+	Entry *currentAddress = _root;
+
+	while (currentAddress) {
+		if (currentAddress->Key == key) {
+			return currentAddress;
+		}
+
+		if (currentAddress->Key >= key) {
+			if (!minAddress || currentAddress->Key < minKey) {
+				minAddress = currentAddress;
+				minKey = currentAddress->Key;
+			}
+		}
+
+		if (currentAddress->Key < key) {
+			currentAddress = currentAddress->Right;
+		} else {
+			currentAddress = currentAddress->Left;
+		}
+	}
+
+	return minAddress;
+}
+
+template <typename KT, typename VT>
+typename Tree<KT, VT>::Entry *Tree<KT, VT>::FindBiggest(const KT &key)
+{
+	Entry *maxAddress = nullptr;
+	KT maxKey;
+
+	Entry *currentAddress = _root;
+
+	while (currentAddress) {
+		if (currentAddress->Key == key) {
+			return currentAddress;
+		}
+
+		if (currentAddress->Key <= key) {
+			if (!maxAddress || currentAddress->Key > maxKey) {
+				maxAddress = currentAddress;
+				maxKey = currentAddress->Key;
+			}
+		}
+
+		if (currentAddress->Key > key) {
+			currentAddress = currentAddress->Left;
+		} else {
+			currentAddress = currentAddress->Right;
+		}
+	}
+
+	return maxAddress;
+}
+
+template <typename KT, typename VT>
+typename Tree<KT, VT>::Entry *Tree<KT, VT>::FindSmallest()
+{
+	Entry *currentAddress = _root;
+
+	while (currentAddress) {
+		if (!currentAddress->Left) {
+			return currentAddress;
+		}
+
+		currentAddress = currentAddress->Left;
+	}
+
+	return nullptr;
+}
+
+template <typename KT, typename VT>
+typename Tree<KT, VT>::Entry *Tree<KT, VT>::FindBiggest()
+{
+	Entry *currentAddress = _root;
+
+	while (currentAddress) {
+		if (!currentAddress->Right) {
+			return currentAddress;
+		}
+
+		currentAddress = currentAddress->Right;
+	}
+
+	return nullptr;
+}
+
+template <typename KT, typename VT>
+typename Tree<KT, VT>::Entry *Tree<KT, VT>::Next(Entry *address)
+{
+	if (address->Right) {
+		address = address->Right;
+
+		while (address->Left) {
+			address = address->Left;
+		}
+
+		return address;
+	}
+
+	for (;;) {
+		Entry *childAddress = address;
+
+		if (!address->Parent) {
+			return nullptr;
+		}
+
+		address = address->Parent;
+
+		if (childAddress == address->Left) {
+			return address;
+		}
+	}
+}
+
+template <typename KT, typename VT>
+typename Tree<KT, VT>::Entry *Tree<KT, VT>::Previous(Entry *address)
+{
+	if (address->Left) {
+		address = address->Left;
+
+		while (address->Right) {
+			address = address->Right;
+		}
+
+		return address;
+	}
+
+	for (;;) {
+		Entry *childAddress = address;
+
+		if (!address->Parent) {
+			return nullptr;
+		}
+
+		address = address->Parent;
+
+		if (childAddress == address->Right) {
+			return address;
+		}
+	}
+}
+
+template <typename KT, typename VT>
+void Tree<KT, VT>::RotateLeft(Entry *address)
+{
+	/* Input argument is the address of node 1.
+	 *
+	 *      1              2
+	 *     / \            / \
+	 *    A   2   --->   1   C
+	 *       / \        / \
+	 *      B   C      A   B
+	 *
+	 *      1              3
+	 *     A \            / \
+	 *        2   --->   1   2
+	 *       / D        A B C D
+	 *      3
+	 *     B C
+	 */
+
+	Entry *address1 = address;
+
+	if (!address1->Right) {
+		return;
+	}
+
+	Entry *address2 = address1->Right;
+
+	Entry *addressA = address1->Left;
+	Entry *addressB = address2->Left;
+	Entry *addressC = address2->Right;
+
+	int32_t depthA = -1;
+	int32_t depthB = -1;
+	int32_t depthC = -1;
+
+	if (addressA) {
+		depthA = addressA->Depth;
+	}
+
+	if (addressB) {
+		depthB = addressB->Depth;
+	}
+
+	if (addressC) {
+		depthC = addressC->Depth;
+	}
+
+	if (depthB > depthC) {
+		RotateRight(address2);
+		RotateLeft(address1);
+		return;
+	}
+
+	Entry *parentAddress = address1->Parent;
+
+	address1->Left = addressA;
+	address1->Right = addressB;
+	address1->Parent = address2;
+	address1->Depth = (depthA > depthB ? depthA : depthB) + 1;
+
+	address2->Left = address1;
+	address2->Right = addressC;
+	address2->Parent = parentAddress;
+	address2->Depth =
+		(address1->Depth > depthC ? address1->Depth : depthC) + 1;
+
+	if (parentAddress) {
+		if (parentAddress->Left == address1) {
+			parentAddress->Left = address2;
+		} else if (parentAddress->Right == address1) {
+			parentAddress->Right = address2;
+		} else {
+			THROW("Parent does not have child address.");
+		}
+	} else {
+		_root = address2;
+	}
+
+	if (addressB) {
+		addressB->Parent = address1;
+	}
+}
+
+template <typename KT, typename VT>
+void Tree<KT, VT>::RotateRight(Entry *address)
+{
+	/* Input argument is the address of node 1.
+	 *
+	 *        1            2
+	 *       / \          / \
+	 *      2   C  --->  A   1
+	 *     / \              / \
+	 *    A   B            B   C
+	 *
+	 *        1            3
+	 *       / \          / \
+	 *      2   D  --->  2   1
+	 *     A \          A B C D
+	 *        3
+	 *       B C
+	 */
+
+	Entry *address1 = address;
+
+	if (!address1->Left) {
+		return;
+	}
+
+	Entry *address2 = address1->Left;
+
+	Entry *addressA = address2->Left;
+	Entry *addressB = address2->Right;
+	Entry *addressC = address1->Right;
+
+	int32_t depthA = -1;
+	int32_t depthB = -1;
+	int32_t depthC = -1;
+
+	if (addressA) {
+		depthA = addressA->Depth;
+	}
+
+	if (addressB) {
+		depthB = addressB->Depth;
+	}
+
+	if (addressC) {
+		depthC = addressC->Depth;
+	}
+
+	if (depthB > depthA) {
+		RotateLeft(address2);
+		RotateRight(address1);
+		return;
+	}
+
+	Entry *parentAddress = address1->Parent;
+
+	address1->Left = addressB;
+	address1->Right = addressC;
+	address1->Parent = address2;
+	address1->Depth = (depthB > depthC ? depthB : depthC) + 1;
+
+	address2->Left = addressA;
+	address2->Right = address1;
+	address2->Parent = parentAddress;
+	address2->Depth =
+		(address1->Depth > depthA ? address1->Depth : depthA) + 1;
+
+	if (parentAddress) {
+		if (parentAddress->Left == address1) {
+			parentAddress->Left = address2;
+		} else if (parentAddress->Right == address1) {
+			parentAddress->Right = address2;
+		} else {
+			THROW("Parent does not have child address.");
+		}
+	} else {
+		_root = address2;
+	}
+
+	if (addressB) {
+		addressB->Parent = address1;
+	}
+}
+
+template <typename KT, typename VT>
+void Tree<KT, VT>::RollUp(Entry *address)
+{
+	while (address) {
+		int32_t leftDepth = 0;
+		int32_t rightDepth = 0;
+
+		if (address->Left) {
+			leftDepth = address->Left->Depth + 1;
+		}
+
+		if (address->Right) {
+			rightDepth = address->Right->Depth + 1;
+		}
+
+		address->Depth =
+			leftDepth > rightDepth ? leftDepth : rightDepth;
+
+		if (rightDepth > leftDepth + 1) {
+			RotateLeft(address);
+		} else if (leftDepth > rightDepth + 1) {
+			RotateRight(address);
+		} else {
+			address = address->Parent;
+		}
+	}
+}
+
+template <typename KT, typename VT>
+void Tree<KT, VT>::Swap(Entry *address1, Entry *address2)
+{
+	/*       P1      P2
+	 *       |       |
+	 *       1       2
+	 *      / \     / \
+	 *     A1 B1   A2 B2
+	 *           |
+	 *           V
+	 *       P2      P1
+	 *       |       |
+	 *       1       2
+	 *      / \     / \
+	 *     A2 B2   A1 B1
+	 *
+	 * If nodes are directly connected, they will form two loops
+	 * that have to be corrected.
+	 *
+	 *       2       P2
+	 *       |       |
+	 *       1       2
+	 *      / \     / \
+	 *     A1 B1   A2  1
+	 *           |
+	 *           V
+	 *       P2      2
+	 *       |       |
+	 *       1       2
+	 *      / \     / \
+	 *     A2  1   A1 B1
+	 *           |
+	 *           V
+	 *       P2      1
+	 *       |       |
+	 *       1       2
+	 *      / \     / \
+	 *     A2  2   A1 B1
+	 */
+
+	if (!address1 || !address2) {
+		return;
+	}
+
+	Entry *P1 = address1->Parent;
+	Entry *P2 = address2->Parent;
+
+	Entry *A1 = address1->Left;
+	Entry *A2 = address2->Left;
+
+	Entry *B1 = address1->Right;
+	Entry *B2 = address2->Right;
+
+	if (A1) {
+		A1->Parent = address2;
+	}
+
+	if (A2) {
+		A2->Parent = address1;
+	}
+
+	if (B1) {
+		B1->Parent = address2;
+	}
+
+	if (B2) {
+		B2->Parent = address1;
+	}
+
+	Entry **P1Ref;
+	Entry **P2Ref;
+
+	if (P1) {
+		if (address1 == P1->Left) {
+			P1Ref = &P1->Left;
+		} else if (address1 == P1->Right) {
+			P1Ref = &P1->Right;
+		} else {
+			THROW("Parent does not have child address.");
+		}
+	} else {
+		P1Ref = &_root;
+	}
+
+	if (P2) {
+		if (address2 == P2->Left) {
+			P2Ref = &P2->Left;
+		} else if (address2 == P2->Right) {
+			P2Ref = &P2->Right;
+		} else {
+			THROW("Parent does not have child address.");
+		}
+	} else {
+		P2Ref = &_root;
+	}
+
+	*P1Ref = address2;
+	*P2Ref = address1;
+
+	address1->Parent = P2;
+	address1->Left = A2;
+	address1->Right = B2;
+
+	address2->Parent = P1;
+	address2->Left = A1;
+	address2->Right = B1;
+
+	// Loop correction.
+	if (address1->Parent == address1) {
+		address1->Parent = address2;
+
+		if (address2->Left == address2) {
+			address2->Left = address1;
+		} else if (address2->Right == address2) {
+			address2->Right = address1;
+		} else {
+			THROW("Loop correction failure.");
+		}
+	}
+
+	if (address2->Parent == address2) {
+		address2->Parent = address1;
+
+		if (address1->Left == address1) {
+			address1->Left = address2;
+		} else if (address1->Right == address1) {
+			address1->Right = address2;
+		} else {
+			THROW("Loop correction failure.");
+		}
+	}
+}
+
+#endif
---
+++ tests/Tree.Test.cpp
@@ -0,0 +1,237 @@
+#include <unistd.h>
+#include <cstdlib>
+#include <cstdio>
+
+#include "../src/Common/Tree.hpp"
+#include "../src/Common/MyString.hpp"
+
+String GetCommand()
+{
+	String res;
+	char c;
+
+	while (read(0, &c, 1) == 1 && c != '\n') {
+		res += c;
+	}
+
+	return res;
+}
+
+int GetDepth(Tree<int, int>::Entry *entry)
+{
+	if (!entry) {
+		return 0;
+	}
+
+	int l = GetDepth(entry->Left);
+	int r = GetDepth(entry->Right);
+
+	return l > r ? l + 1 : r + 1;
+}
+
+void FillLayer(
+	Tree<int, int>::Entry *entry,
+	int layer,
+	int *array,
+	int *depth,
+	int *index)
+{
+	if (layer) {
+		if (entry) {
+			FillLayer(entry->Left, layer - 1, array, depth, index);
+			FillLayer(entry->Right, layer - 1, array, depth, index);
+		} else {
+			FillLayer(nullptr, layer - 1, array, depth, index);
+			FillLayer(nullptr, layer - 1, array, depth, index);
+		}
+
+		return;
+	}
+
+	if (!entry) {
+		array[*index] = 0;
+		depth[*index] = -1;
+	} else {
+		array[*index] = entry->Key;
+		depth[*index] = entry->Depth;
+	}
+
+	*index += 1;
+}
+
+void PrintLayer(
+	Tree<int, int>::Entry *entry,
+	int layer,
+	int depth)
+{
+	int itemCount = 1;
+
+	for (int i = 0; i < layer; i++) {
+		itemCount *= 2;
+	}
+
+	CowBuffer<int> vals(itemCount);
+	CowBuffer<int> depths(itemCount);
+	int index = 0;
+	FillLayer(entry, layer, vals.Pointer(), depths.Pointer(), &index);
+
+	int offset = 3;
+	int startOffset = 0;
+
+	for (int i = depth; i > layer; i--) {
+		startOffset += offset / 2 + 1;
+		offset = offset + 2 + offset / 2 * 2;
+	}
+
+	for (int i = 0; i < startOffset; i++) {
+		write(1, " ", 1);
+	}
+
+	for (int val = 0; val < (int)vals.Size(); val++) {
+		int cp;
+
+		if (vals[val]) {
+			cp = printf("%d", vals[val]);
+		} else {
+			cp = printf(" ");
+		}
+
+		fflush(stdout);
+
+		int locOffset = offset + 1 - cp;
+
+		for (int i = 0; i < locOffset; i++) {
+			write(1, " ", 1);
+		}
+	}
+
+	write(1, "\n", 1);
+
+	for (int i = 0; i < startOffset; i++) {
+		write(1, " ", 1);
+	}
+
+	for (int val = 0; val < (int)depths.Size(); val++) {
+		int cp;
+
+		if (depths[val] >= 0) {
+			cp = printf("%d", depths[val]);
+		} else {
+			cp = printf(" ");
+		}
+
+		fflush(stdout);
+
+		int locOffset = offset + 1 - cp;
+
+		for (int i = 0; i < locOffset; i++) {
+			write(1, " ", 1);
+		}
+	}
+
+	write(1, "\n", 1);
+}
+
+void DumpTree(Tree<int, int> &tree)
+{
+	Tree<int, int>::Entry *root = tree.FindSmallest();
+
+	if (!root) {
+		return;
+	}
+
+	while (root->Parent) {
+		root = root->Parent;
+	}
+
+	int depth = GetDepth(root);
+
+	for (int i = 0; i < depth; i++) {
+		PrintLayer(root, i, depth);
+	}
+}
+
+void ListTree(Tree<int, int> &tree)
+{
+	Tree<int, int>::Entry *current = tree.FindSmallest();
+
+	while (current) {
+		printf(" %d", current->Key);
+		current = tree.Next(current);
+	}
+
+	printf("\n");
+}
+
+void RListTree(Tree<int, int> &tree)
+{
+	Tree<int, int>::Entry *current = tree.FindBiggest();
+
+	while (current) {
+		printf(" %d", current->Key);
+		current = tree.Previous(current);
+	}
+
+	printf("\n");
+}
+
+int main(int argc, char **argv)
+{
+	Tree<int, int> tree;
+
+	String command;
+
+	while ((command = GetCommand()).Length()) {
+		if (command == "exit") {
+			break;
+		}
+
+		CowBuffer<String> args = command.Split(' ', true);
+
+		if (!args.Size()) {
+			continue;
+		}
+
+		if (args[0] == "add") {
+			if (args.Size() != 2) {
+				printf("No argument given.\n");
+				continue;
+			}
+
+			bool res = tree.AddEntry(atoi(args[1].CStr()), 0);
+
+			if (res) {
+				printf("Added.\n");
+			} else {
+				printf("Duplicate.\n");
+			}
+
+
+		} else if (args[0] == "del") {
+			if (args.Size() != 2) {
+				printf("No argument given.\n");
+				continue;
+			}
+
+			Tree<int, int>::Entry *entry =
+				tree.FindEntry(atoi(args[1].CStr()));
+
+			if (!entry) {
+				printf("Not found.\n");
+			} else {
+				tree.RemoveEntry(entry);
+				printf("Removed.\n");
+			}
+		} else if (args[0] == "dump") {
+			DumpTree(tree);
+		} else if (args[0] == "list") {
+			ListTree(tree);
+		} else if (args[0] == "rlist") {
+			RListTree(tree);
+		} else {
+			printf("Unknown command.\n");
+		}
+	}
+
+	return 0;
+}
